7085e4102e6f7e57983fb0c4a0de79d6c0348e28
Patrick Stahl <p.stahl@shopware.com>
2019-04-18 14:04:22 +0200

    Fix outdated Rule method head

--- src/Core/Rule/LunarEclipseRule.php
+++ src/Core/Rule/LunarEclipseRule.php
@@ -2,7 +2,6 @@
 
 namespace Swag\CustomRule\Core\Rule;
 
-use Shopware\Core\Framework\Rule\Match;
 use Shopware\Core\Framework\Rule\Rule;
 use Shopware\Core\Framework\Rule\RuleScope;
 use Symfony\Component\Validator\Constraints\Type;
@@ -25,7 +24,7 @@ class LunarEclipseRule extends Rule
         return 'lunar_eclipse';
     }
 
-    public function match(RuleScope $scope): Match
+    public function match(RuleScope $scope): bool
     {
         // Not implemented in this example
         $isCurrentlyLunarEclipse = $this->isCurrentlyLunarEclipse();
@@ -33,11 +32,11 @@ class LunarEclipseRule extends Rule
         // Checks if the shop administrator set the rule to "Lunar eclipse => Yes"
         if ($this->isLunarEclipse) {
             // Shop administrator wants the rule to match if there's currently a lunar eclipse.
-            return new Match($isCurrentlyLunarEclipse, ['Message if not lunar eclipse']);
+            return $isCurrentlyLunarEclipse;
         }
 
         // Shop administrator wants the rule to match if there's currently NOT a lunar eclipse.
-        return new Match(!$isCurrentlyLunarEclipse, ['Message if lunar eclipse']);
+        return !$isCurrentlyLunarEclipse;
     }
 
     public function getConstraints(): array
--- tests/UsedClassesAvailableTest.php
+++ tests/UsedClassesAvailableTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Swag\CustomServiceTests;
+namespace Swag\CustomRuleTests;
 
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 use PHPUnit\Framework\TestCase;
